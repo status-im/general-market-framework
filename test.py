15faaec240f0a154a89e70c0ba86d99489b219f0
Jarrad Hope <me@jarradhope.com>
2015-07-16 17:28:18 +0200

    setup basic buy and sell tickets

--- test.py
+++ test.py
@@ -5,21 +5,33 @@ import os
 
 state = tester.state()
 
-
-def listener(msg):
-    if msg['event'] == 'LOG':
-        print(msg['topics'])
-
-slogging.log_listeners.listeners.append(listener)
-
+# Create Market Contract
 market = state.abi_contract('contracts/market.se', gas=10000000)
 
 
+# Create Match Maker
+class Matchmaker:
+    def listener(self, msg):
+        if msg['event'] == 'LOG':
+            print('announce', msg['topics'])
 
-# ticket = market.add()
-# print(ticket)
-# ticket2 = market.add()
-# print(ticket)
+    def __init__(self):
+        slogging.log_listeners.listeners.append(self.listener)
+
+match_maker = Matchmaker()
+
+# Create buy ticket, add preferences, activate
+buy_ticket = market.add()
+market.add_preference(buy_ticket, "price", 5)
+market.activate(buy_ticket)
+
+# Create sell ticket, add preferences, activate
+sell_ticket = market.add()
+market.add_preference(sell_ticket, "price", -5)
+market.activate(sell_ticket)
+
+
+
 # head = tail = xorll.insert("head", 10, 0, 0)
 # tail = xorll.insert("tail", 20, 0, tail)
 
