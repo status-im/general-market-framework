0154c4adddb9abfa66808708fbd966d0ba310e15
Jarrad Hope <me@jarradhope.com>
2015-07-18 23:06:17 +0200

    less expensive xorll addr

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ market.activate(buy_ticket)
 # Create sell ticket, add preferences, activate
 sell_ticket = market.add()
 print('sell_ticket', sell_ticket)
-market.add_preference(sell_ticket, 'price', '-7') # TODO: handle negative numbers
+market.add_preference(sell_ticket, 'price', '-7')  # TODO: handle negative numbers
 market.activate(sell_ticket)
 
 # print('reveal', market.add_sealed_offer(0, 1))
