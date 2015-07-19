a6b490e4f072ba6df21024c14ddc1ed7fd8f2643
Jarrad Hope <me@jarradhope.com>
2015-07-19 11:03:02 +0200

    return preferences as array

--- test.py
+++ test.py
@@ -26,19 +26,21 @@ match_maker = Matchmaker()
 # Create buy ticket, add preferences, activate
 buy_ticket = market.add()
 print('buy_ticket', buy_ticket)
-market.add_preference(buy_ticket, 'price', '5')
+market.add_preference(buy_ticket, 'head', 10)
+market.add_preference(buy_ticket, 'tail', 20)
+market.add_preference(buy_ticket, 'price', 5)
 market.activate(buy_ticket)
 
 # Create sell ticket, add preferences, activate
 sell_ticket = market.add()
 print('sell_ticket', sell_ticket)
-market.add_preference(sell_ticket, 'price', '-7')  # TODO: handle negative numbers
+market.add_preference(sell_ticket, 'price', -5)
 market.activate(sell_ticket)
 
 # print('reveal', market.add_sealed_offer(0, 1))
 
 print('enumerate buy_ticket preferences')
-market.get_preferences(buy_ticket)
+print(market.get_preferences(buy_ticket))
 
 print('enumerate sell_ticket preferences')
-market.get_preferences(sell_ticket)
+print(market.get_preferences(sell_ticket))
