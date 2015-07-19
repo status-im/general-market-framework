fbfea0708959fbd18a385812b4bed0e1b3ad9130
Jarrad Hope <me@jarradhope.com>
2015-07-19 18:41:21 +0200

    niave matcher and add sealed offer

--- test.py
+++ test.py
@@ -2,6 +2,9 @@
 
 from ethereum import tester, slogging, utils
 import os
+from collections import namedtuple
+
+Ticket = namedtuple("Ticket", "id owner epoch preferences")
 
 state = tester.state()
 
@@ -13,29 +16,50 @@ market = state.abi_contract('contracts/market.se', gas=10000000)
 # Create Match Maker
 class Matchmaker:
 
+    buyers = []
+    sellers = []
+    current_block = -1
+
+    def make_match(self, buyer, seller):
+        # add sealed offers
+        shasum = utils.sha3([buyer, seller])
+        print('making match', buyer, seller)
+        print('hash', shasum)
+        print('reveal', market.add_sealed_offer(buyer.id, shasum))
+
     def process(self):
         # TODO:
-        # make matches
-        # add sealed offers
+        # naively make matches
+        for s in self.sellers:
+            for b in self.buyers:
+                for k, v in s.preferences.viewitems() & b.preferences.viewitems():
+                    print('match found on', k, v)
+                    self.sellers.remove(s)
+                    self.buyers.remove(b)
+                    self.make_match(b, s)
         # reveal offers
         # do cleanup
-        # print('reveal', market.add_sealed_offer(0, 1))
         print('processing on block', self.current_block)
 
     def announce(self, data):
         ''' A new ticket has arrived '''
-        ticket = data[0]
-        info = market.get_info(ticket)
+        ticket_id = data[0]
+        info = market.get_info(ticket_id)
 
         # Rebuild Preferences
-        preferences = market.get_preferences(ticket)
+        preferences = market.get_preferences(ticket_id)
         keys = [utils.encode_int(x) for x in preferences[::2]]
         preferences = dict(zip(keys, preferences[1::2]))
         del preferences['']
 
-        print('ticket #', ticket)
-        print('ticket info', info)  # owner, epoch, state
-        print('ticket preferences', preferences)
+        ticket = Ticket(ticket_id, info[0], info[1], preferences)
+
+        # Our match maker assumes there is a price
+        if ticket.preferences['price'] < 0:
+            ticket.preferences['price'] = abs(ticket.preferences['price'])
+            self.sellers.append(ticket)
+        else:
+            self.buyers.append(ticket)
 
         self.process()
 
@@ -53,10 +77,10 @@ class Matchmaker:
         if event == 'LOG' and msg['to'] == self.market:
             msg_type = utils.encode_int(msg['topics'][0]).rstrip('\x00')
             msg_data = msg['topics'][1:]
-            try:
-                getattr(self, msg_type)(msg_data)
-            except:
-                raise NotImplementedError(msg_type, msg_data)
+            # try:
+            getattr(self, msg_type)(msg_data)
+            # except:
+            #     raise NotImplementedError(msg_type, msg_data)
         elif event == 'delta':
             if self.current_block < state.block.number:
                 self.current_block = state.block.number
@@ -64,7 +88,6 @@ class Matchmaker:
 
     def __init__(self, market):
         self.market = market
-        self.current_block = -1
         slogging.log_listeners.listeners.append(self.listener)
 
 # TODO: Confirm this is correct?
